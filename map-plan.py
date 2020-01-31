6b7f1972d0be52aab1a3bcd9e68a27b73ef5e0fe
pc-pt <prateek.chhikara@proptiger.com>
2020-01-31 15:03:17 +0530

    Second commit

---
+++ map-plan.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+import time
+import argparse
+import random
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from torch.autograd import Variable
+
+from PIL import Image
+
+import cv2
+from skimage import io
+import numpy as np
+import craft_utils
+import imgproc
+import file_utils
+import json
+import zipfile
+import logging
+logger=logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter=logging.Formatter('%(levelname)s:%(asctime)s:%(funcName)s:%(lineno)d:%(message)s')
+file_handler=logging.FileHandler('craft-pytorch.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+
+
+logger.info("Testing on --> /mnt/sdc_ssd/datasets/pt/labelled_data/image/makaan_good_images_6_classes/living/furnished.....")
+logger.info("text_threshold=0.7, link_threshold=0.4, low_text=0.4")
+from craft import CRAFT
+
+from collections import OrderedDict
+def copyStateDict(state_dict):
+    if list(state_dict.keys())[0].startswith("module"):
+        start_idx = 1
+    else:
+        start_idx = 0
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = ".".join(k.split(".")[start_idx:])
+        new_state_dict[name] = v
+    return new_state_dict
+
+def str2bool(v):
+    return v.lower() in ("yes", "y", "true", "t", "1")
+
+parser = argparse.ArgumentParser(description='CRAFT Text Detection')
+parser.add_argument('--trained_model', default='craft_mlt_25k.pth', type=str, help='pretrained model')
+parser.add_argument('--text_threshold', default=0.7, type=float, help='text confidence threshold')
+parser.add_argument('--low_text', default=0.4, type=float, help='text low-bound score')
+parser.add_argument('--link_threshold', default=0.4, type=float, help='link confidence threshold')
+parser.add_argument('--cuda', default=True, type=str2bool, help='Use cuda for inference')
+parser.add_argument('--canvas_size', default=1280, type=int, help='image size for inference')
+parser.add_argument('--mag_ratio', default=1.5, type=float, help='image magnification ratio')
+parser.add_argument('--poly', default=False, action='store_true', help='enable polygon type')
+parser.add_argument('--show_time', default=False, action='store_true', help='show processing time')
+parser.add_argument('--test_folder', default='/mnt/sdc_ssd/datasets/pt/labelled_data/image/makaan_good_images_6_classes/living/furnished', type=str, help='folder path to input images')
+parser.add_argument('--refine', default=False, action='store_true', help='enable link refiner')
+parser.add_argument('--refiner_model', default='weights/craft_refiner_CTW1500.pth', type=str, help='pretrained refiner model')
+
+args = parser.parse_args()
+os.environ['CUDA_VISIBLE_DEVICES'] = "7"
+
+import pickle
+with open("shuffle-output-text.txt", "rb") as fp:   # Unpickling
+    b = pickle.load(fp)
+
+
+""" For test images in a folder """
+image_list, _, _ = file_utils.get_files(args.test_folder)
+# validation_set = b[:750]
+# test_set= b[:]
+
+
+# result_folder = './result/'
+# if not os.path.isdir(result_folder):
+#     os.mkdir(result_folder)
+
+def test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly, refine_net=None):
+    t0 = time.time()
+
+    # resize
+    img_resized, target_ratio, size_heatmap = imgproc.resize_aspect_ratio(image, args.canvas_size, interpolation=cv2.INTER_LINEAR, mag_ratio=args.mag_ratio)
+    ratio_h = ratio_w = 1 / target_ratio
+
+    # preprocessing
+    x = imgproc.normalizeMeanVariance(img_resized)
+    x = torch.from_numpy(x).permute(2, 0, 1)    # [h, w, c] to [c, h, w]
+    x = Variable(x.unsqueeze(0))                # [c, h, w] to [b, c, h, w]
+    if cuda:
+        x = x.cuda()
+
+    # forward pass
+    with torch.no_grad():
+        y, feature = net(x)
+
+    # make score and link map
+    score_text = y[0,:,:,0].cpu().data.numpy()
+    score_link = y[0,:,:,1].cpu().data.numpy()
+
+    # refine link
+    if refine_net is not None:
+        with torch.no_grad():
+            y_refiner = refine_net(y, feature)
+        score_link = y_refiner[0,:,:,0].cpu().data.numpy()
+
+    t0 = time.time() - t0
+    t1 = time.time()
+
+    # Post-processing
+    boxes, polys = craft_utils.getDetBoxes(score_text, score_link, text_threshold, link_threshold, low_text, poly)
+
+    # coordinate adjustment
+    boxes = craft_utils.adjustResultCoordinates(boxes, ratio_w, ratio_h)
+    polys = craft_utils.adjustResultCoordinates(polys, ratio_w, ratio_h)
+    for k in range(len(polys)):
+        if polys[k] is None: polys[k] = boxes[k]
+
+    t1 = time.time() - t1
+
+    # render results (optional)
+    render_img = score_text.copy()
+    render_img = np.hstack((render_img, score_link))
+    ret_score_text = imgproc.cvt2HeatmapImg(render_img)
+
+    if args.show_time : print("\ninfer/postproc time : {:.3f}/{:.3f}".format(t0, t1))
+
+    return boxes, polys, ret_score_text
+
+
+
+if __name__ == '__main__':
+    # load net
+    net = CRAFT()     # initialize
+
+    logger.info('Loading weights from checkpoint (' + args.trained_model + ')')
+    if args.cuda:
+        net.load_state_dict(copyStateDict(torch.load(args.trained_model)))
+    else:
+        net.load_state_dict(copyStateDict(torch.load(args.trained_model, map_location='cpu')))
+
+    if args.cuda:
+        net = net.cuda()
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = False
+
+    net.eval()
+
+    # LinkRefiner
+    refine_net = None
+    if args.refine:
+        from refinenet import RefineNet
+        refine_net = RefineNet()
+        logger.info('Loading weights of refiner from checkpoint (' + args.refiner_model + ')')
+        if args.cuda:
+            refine_net.load_state_dict(copyStateDict(torch.load(args.refiner_model)))
+            refine_net = refine_net.cuda()
+            refine_net = torch.nn.DataParallel(refine_net)
+        else:
+            refine_net.load_state_dict(copyStateDict(torch.load(args.refiner_model, map_location='cpu')))
+
+        refine_net.eval()
+        args.poly = True
+
+    t = time.time()
+    zz=0
+    nz=0
+    # load data
+    from tqdm import tqdm
+    # image_list=tqdm(image_list)
+    for k, image_path in enumerate(tqdm(image_list)):
+        # logger.info("Test image {:d}/{:d}: {:s}".format(k+1, len(image_list), image_path))
+        try:
+            image = imgproc.loadImage(image_path)
+            # cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+            # image = cv2.blur(image,(10,10))
+            bboxes, polys, score_text = test_net(net, image, args.text_threshold, args.link_threshold, args.low_text, args.cuda, args.poly, refine_net)
+            if(len(bboxes)==0):
+                filename, file_ext = os.path.splitext(os.path.basename(image_path))
+                file_utils.saveResult(image_path, image[:,:,::-1], polys, dirname="./resA/non_text/")
+                zz=zz+1
+            else:
+                nz=nz+1
+                filename, file_ext = os.path.splitext(os.path.basename(image_path))
+                file_utils.saveResult(image_path, image[:,:,::-1], polys, dirname="./resA/text/")
+            # save score text
+            # filename, file_ext = os.path.splitext(os.path.basename(image_path))
+            # mask_file = result_folder + "/res_" + filename + '_mask.jpg'
+            # # cv2.imwrite(mask_file, score_text)
+            # file_utils.saveResult(image_path, image[:,:,::-1], polys, dirname=result_folder)
+            if(k%25==0):
+                logger.info("text={}, non-text={}, total={}".format(nz,zz,zz+nz))
+        except: 
+            logger.exception("Corrupt Image: couldn't open. "+ str(image))
+
+    logger.info("final results  ::  -->> text={}, non-text={}, total={}".format(nz,zz,zz+nz))
+    logger.info("elapsed time : {}s".format(time.time() - t))
+    # logger.info("frame per second : {}s".format(k/(time.time() - t)))
